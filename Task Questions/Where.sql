72d15fbd172a7a18e099f39e27def9ea056c5baa
Karthika Raj <148317926+Codewithkarthi@users.noreply.github.com>
2024-03-14 15:53:30 +0530

    Update Where.sql

--- Task Questions/Where.sql
+++ Task Questions/Where.sql
@@ -5,7 +5,6 @@ WHERE City = 'chennai';
 
 /* Question: Select all records where the City column has the value 'Berlin' or 'London'.*/
 
-
 SELECT * FROM Customers
 WHERE City = 'Berlin'
 OR City = 'London';

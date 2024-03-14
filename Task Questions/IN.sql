e809f0236a0fe96aff6879e0dd3129ec2288648e
Karthika Raj <148317926+Codewithkarthi@users.noreply.github.com>
2024-03-14 15:47:28 +0530

    Update IN.sql

--- Task Questions/IN.sql
+++ Task Questions/IN.sql
@@ -2,3 +2,8 @@
 
 SELECT * FROM Customers
 WHERE Country IN('Norway', 'France');
+
+/*Use the IN operator to select all the records where Country is NOT "Norway" and NOT "France".*/
+
+SELECT * FROM Customers
+WHERE Country NOT IN('Norway','France');

8a6b7c1f47416cd499b38c36ba8e36830bf075af
Karthika Raj <148317926+Codewithkarthi@users.noreply.github.com>
2024-03-16 15:43:34 +0530

    Update LEFT JOIN.sql

--- JOINS/LEFT JOIN.sql
+++ JOINS/LEFT JOIN.sql
@@ -4,3 +4,10 @@ FROM table1
 LEFT JOIN table2
 ON table1.column_name = table2.column_name;*/
 
+/*Insert the missing parts in the JOIN clause to join the two tables Orders and Customers, using the CustomerID field in both tables as the relationship between the two tables.*/
+
+SELECT *
+FROM Orders
+LEFT JOIN Customers
+ON Orders.CustomerID=
+Customers.CustomerID;
